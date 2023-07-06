6d82ac4ef06f55a0bcda2b1f30cc334b87e487af
Measurity <measuring.infnity@gmail.com>
2023-07-06 21:52:51 +0200

    Moved from NET 3.1 to NET 7.0 in all projects

--- samples/BinaryPack.Benchmark/BuildPlots.bat
+++ samples/BinaryPack.Benchmark/BuildPlots.bat
@@ -1,1 +1,1 @@
-"%R_HOME%\bin\Rscript.exe" "BuildPlots_Nitrox.R" "bin\Release\netcoreapp3.1\BenchmarkDotNet.Artifacts\results\BinaryPack.Benchmark.Implementations.Benchmark_JsonResponseModel_-report.csv"
+"%R_HOME%\bin\Rscript.exe" "BuildPlots_Nitrox.R" "bin\Release\net7\BenchmarkDotNet.Artifacts\results\BinaryPack.Benchmark.Implementations.Benchmark_JsonResponseModel_-report.csv"
